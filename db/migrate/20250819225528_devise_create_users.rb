f6e46fe7d74b8ee11242a8fd5eb2cd59e1bd3406
Victor Manzanilla <67130786+VictorManzanilla@users.noreply.github.com>
2025-08-22 01:08:03 +0000

    edit users table email column citext

--- db/migrate/20250819225528_devise_create_users.rb
+++ db/migrate/20250819225528_devise_create_users.rb
@@ -4,7 +4,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.1]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
+      enable_extension("citext")
+      t.citext :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,7 @@
 
 ActiveRecord::Schema[7.1].define(version: 2025_08_19_233424) do
   # These are extensions that must be enabled in order to support this database
+  enable_extension "citext"
   enable_extension "plpgsql"
 
   create_table "conversation_ais", force: :cascade do |t|
@@ -37,7 +38,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_08_19_233424) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
+    t.citext "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
