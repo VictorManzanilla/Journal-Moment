6b2c34926259c0bb0b9d45e30698348e7ce28495
Victor Manzanilla <67130786+VictorManzanilla@users.noreply.github.com>
2025-08-29 02:39:35 +0000

    added force sign up, partials for navbar for bootstrap, and organized routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  resources :journal_entries
-  devise_for :users
- root to: "journal_entries#index"
-  # This is a blank app! Pick your first screen, build out the RCAV, and go from there. E.g.:
+ root "journal_entries#index"
 
-  # get "/your_first_screen" => "pages#first"
+  devise_for :users
+  resources :journal_entries
+  
+ 
   
 end
