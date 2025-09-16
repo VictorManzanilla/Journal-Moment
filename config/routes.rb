5e9fd2ad2825a2fcab124549e7da4e31e5ad2402
Victor Manzanilla <67130786+VictorManzanilla@users.noreply.github.com>
2025-09-16 16:00:25 +0000

    moved simple calendar gem out of dev and cleaned routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,25 @@
-Rails.application.routes.draw do
-  require "sidekiq/web"
-  root to: "journal_entries#index"
+ require "sidekiq/web"
+
+ Rails.application.routes.draw do
+ root to: "journal_entries#index"
 
 
   devise_for :users
- resources :emotions, only: [:new, :create] do
-  collection do
-    get :thank_you
-  end
-end
 
- 
+  resources :emotions, only: [:new, :create] do
+    collection do
+      get :thank_you
+    end
+  end
+
   resources :journal_entries do
     resources :conversation_ais, only: [:create]
   end
   
- if Rails.env.development?
-  mount LetterOpenerWeb::Engine, at: "/letter_opener"
-end
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
 
-
-mount Sidekiq::Web => "/sidekiq"
+  mount Sidekiq::Web => "/sidekiq"
 
 end
