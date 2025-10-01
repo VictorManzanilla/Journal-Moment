12f97b2c9a38d390fc5512bc6b2781a6863de73c
Victor Manzanilla <67130786+VictorManzanilla@users.noreply.github.com>
2025-10-01 21:54:27 +0000

    organizing files and added new layout for devise login and register new

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,9 @@
 class PagesController < ApplicationController
   # NOTE: Make sure to utilize only one application.html.erb. Just follow RCAV principles so you don't need to force your app to load specific templates.
   layout "landing_page", only: [:landing]
-
   skip_before_action :authenticate_user!, only: :landing
 
   def landing
   end
+  
 end
